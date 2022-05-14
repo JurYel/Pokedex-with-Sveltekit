1830aa9027e03001fcc562052012e361f018c4c3
John Ryan Aparece <japarece275@gmail.com>
2022-05-14 21:45:26 +0800

    [POKEDEX-001] - Initial Commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+    <title>About the Svelte Kit Pokedex</title>
+</svelte:head>
+<h1>About</h1>
+<p>Svelte Kit Demo that uses Pokemon API</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+    console.log("Hello world!");
+</script>
+<svelte:head>
+    <title>
+        Svelte Kit Pokedex
+    </title>
+</svelte:head>
+<h1>Svelte Kit Pokedex</h1>
+<style>
+    h1 {
+        color: red;
+    }
+</style>
