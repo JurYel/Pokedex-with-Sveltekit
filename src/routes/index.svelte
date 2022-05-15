4b10e4e3c36d935b697780d4da9fa50acfa74616
Juriel Botoy <juriel.botoy182@gmail.com>
2022-05-15 17:35:05 +0800

    [POKEDEX-004] - Added Search and Svelte Binding

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,17 @@
 <script>
     import {pokemon} from "../stores/pokestore";
     import PokemanCard from "../components/pokemanCard.svelte";
-    console.log($pokemon);
+    
+    let searchTerm = "";
+    let filteredPokemon = [];
+
+    $: {
+        if(searchTerm){
+            filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        }else {
+            filteredPokemon = [...$pokemon];
+        }
+    }
 </script>
 <svelte:head>
     <title>
@@ -10,9 +20,11 @@
 </svelte:head>
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
+<input type="text" placeholder="Search Pokemon" bind:value={searchTerm} class="w-full rounded-md text-lg p-4 border-2 border-gray-200">
+
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
 
-    {#each $pokemon as pokem}
+    {#each filteredPokemon as pokem}
         <PokemanCard pokeman={pokem} />
     {/each}
 
